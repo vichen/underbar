b9125498b64747db09a39d1831ab31e2016f2d6b
Vivian Chen <vichen@vassar.edu>
2016-01-10 20:57:29 -0700

    _.every

--- src/underbar.js
+++ src/underbar.js
@@ -199,6 +199,15 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(allMatch, item) {
+    	if (iterator == undefined) {
+    		if (!item) return false;
+    	} else if (!iterator(item)) {
+    		allMatch = false;
+    		return allMatch;
+    	}
+    	return allMatch;
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
